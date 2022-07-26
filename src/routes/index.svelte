7109c275bc68d207e61e3c2072ddcf7b5feaf2dd
Allan Rodriguez <0x.allan@gmail.com>
2022-07-26 01:25:21 -0600

    index placeholder created

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col justify-center items-center h-screen bg-orange-400">
+  <span class="text-9xl text-white">
+    2A Digital Studio
+  </span>
+
+  <div class="my-5"></div>
+
+  <span class="text-lg font-light">
+    A new era from Digital Marketing in Guatemala is comming soon...
+  </span>
+</div>
